86449734f3518478b801809316aca8165db7a865
haroon <haroon.seena2017@gmail.com>
2023-06-17 21:30:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1;
+let num3 = 24;
+let num4 = 5;
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 1) + 1
+const mod = num3 % num4
+const max = Math.max(5, 10, 15, 20)
+
+console.log(max)
